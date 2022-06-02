0f895fa285bc60e6aa124f96e3b4167eb2f2f578
Ankit Thakur <ankithakur3110@gmail.com>
2022-06-02 17:59:43 +0530

    added server code with express-fileupload

--- ocr-node-application/Routes/MainRoute.js
+++ ocr-node-application/Routes/MainRoute.js
@@ -1,8 +1,48 @@
-const express = require('express');
+const express = require("express");
+const tesseract = require("tesseract.js");
 
 const Router = express.Router();
-const MainController = require('../Controllers/MainController');
+const SUPPORTED_IMAGE_TYPES = [
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "mage/pipeg",
+  "image/tiff",
+  "image/png",
+];
 
-Router.get('/ocr', MainController.getOCR)
+const isValidImage = (file) => SUPPORTED_IMAGE_TYPES.includes(file.mimetype);
+
+const getOCR = async (req, res) => {
+  try {
+    if (req.files === null) {
+      return res.status(400).json({ msg: "No file uploaded" });
+    }
+    const file = req.files.file;
+
+    if (!isValidImage(file)) {
+      return res.status(400).json({ msg: "Format not supported" });
+    }
+    file.mv(`${__dirname}/images/${file.name}`, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+
+      tesseract
+        .recognize(`${__dirname}/images/${file.name}`, "eng")
+        .then((output) => {
+          res.status(200).json({ output: output.data });
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+Router.post("/uploadfile", getOCR);
 
 module.exports = Router;
--- ocr-ui-application/src/App.js
+++ ocr-ui-application/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import FormComponent from './FormComponent';
+import React from "react";
+import "./App.css";
+import FileUpload from "./FormComponent";
 
-function App() {
-  return (
-    <div className="App">
-        <FormComponent />
-    </div>
-  );
-}
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">File Upload</h4>
+    <FileUpload />
+  </div>
+);
 
 export default App;
